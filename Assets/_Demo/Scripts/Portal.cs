6115a050396cc8b17e08a77ccd9eddbe1c632a6d
Polaris00418 <35550887+Polaris00418@users.noreply.github.com>
2018-12-19 02:52:16 +0800

    Initial commit

---
+++ Assets/_Demo/Scripts/Portal.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Portal : MonoBehaviour {
+
+	[Header("Cameras")]
+	public Camera portalCam;
+
+
+	void Update () {
+
+		// Move portal camera position based on main camera distance from the portal.
+		Vector3 cameraOffset = Camera.main.transform.position - transform.position;
+		portalCam.transform.position = transform.position + cameraOffset;
+
+		// Make portal cam face the same direction as the main camera.
+		portalCam.transform.rotation = Quaternion.LookRotation(Camera.main.transform.forward, Vector3.up);
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.CompareTag ("MainCamera")) {
+
+			// Use xor operator to toggle the ARWorld layer in the mainCam's culling mask.
+			Camera.main.cullingMask ^= 1 << LayerMask.NameToLayer("ARWorld");
+
+		}
+	}
+}
---
+++ Assets/_Demo/Scripts/PortalControl.cs
@@ -0,0 +1,71 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.Video;
+
+//public class PortalControl : MonoBehaviour {
+//	public VideoClip[] clips;
+//	VideoPlayer mPlayer;
+//	int _curVideoIndex;
+//
+//	public void NextVideo(){
+//		if (++_curVideoIndex == clips.Length) {
+//			_curVideoIndex = 0;
+//		}
+//		mPlayer = GameObject.FindObjectOfType<VideoPlayer> ();
+//		if (mPlayer == null)
+//			return;
+//		mPlayer.Stop();
+//		mPlayer.clip = clips[_curVideoIndex];
+//		mPlayer.Play ();
+//	}
+//	public void PrevVideo(){
+//		if (--_curVideoIndex == -1) {
+//			_curVideoIndex = clips.Length - 1;
+//		}
+//		mPlayer = GameObject.FindObjectOfType<VideoPlayer> ();
+//		mPlayer = GameObject.FindObjectOfType<VideoPlayer> ();
+//		if (mPlayer == null)
+//			return;
+//		mPlayer.Stop();
+//		mPlayer.clip = clips[_curVideoIndex];
+//		mPlayer.Play ();
+//	}
+//
+//}
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RenderHeads.Media.AVProVideo;
+
+public class PortalControl : MonoBehaviour {
+	public string[] videoPaths;
+	public MediaPlayer mPlayer;
+	int _curVideoIndex;
+
+	public void NextVideo(){
+		if (++_curVideoIndex == videoPaths.Length) {
+			_curVideoIndex = 0;
+		}
+		mPlayer.Stop();
+		mPlayer.OpenVideoFromFile (MediaPlayer.FileLocation.RelativeToStreamingAssetsFolder, videoPaths [_curVideoIndex]);
+	}
+	public void PrevVideo(){
+		if (--_curVideoIndex == -1) {
+			_curVideoIndex = videoPaths.Length - 1;
+		}
+		mPlayer.Stop();
+		mPlayer.OpenVideoFromFile (MediaPlayer.FileLocation.RelativeToStreamingAssetsFolder, videoPaths [_curVideoIndex]);
+	}
+	// Use this for initialization
+	void Start () {
+		_curVideoIndex = 1;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
